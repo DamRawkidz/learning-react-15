492552c7d149ea633be88340cd15cbcbc9c4ce2a
Phet <rawkidz@hotmail.com>
2024-09-15 02:08:39 +0700

    [update] add state isloading

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -4,14 +4,20 @@ import Places from './Places.jsx';
 const places = localStorage.getItem('places')
 
 export default function AvailablePlaces({ onSelectPlace }) {
-
-  const [AvailablePlaces, setAvailablePlaces] = useState(places)
+  const [isFetching, setIsFetching] = useState(false)
+  const [AvailablePlaces, setAvailablePlaces] = useState([])
   useEffect(() => {
-    fetch('http://localhost:3000/places').then((response) => {
-      return response.json()
-    }).then((resData) => {
+
+    async function featchPlaces() {
+      setIsFetching(true)
+      const resposen = await fetch('http://localhost:3000/places')
+      const resData = await resposen.json()
       setAvailablePlaces(resData.places)
-    })
+      setIsFetching(false)
+    }
+
+    featchPlaces()
+
   }, [])
 
 
@@ -19,6 +25,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
   return (
     <Places
       title="Available Places"
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       places={AvailablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
